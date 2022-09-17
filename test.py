5450725e9eac79e0468c588ff0184a4ad38d0027
im-xga <73315016+im-xga@users.noreply.github.com>
2022-09-17 11:23:00 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-num = input('Уведіть номер Вашої маршрутки:')
-if num.isdigit():
-    num_sequense = re.sub(r'(\n)(?=\n)', r'\2 ', num)
-    print("Ваша маршрутка:", num_sequense)
-else:
-    print("Тільки цифри!", num)
