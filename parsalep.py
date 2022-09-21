832f1008a9ea5fc94662bad71933c13fa43da0bb
im-xga <73315016+im-xga@users.noreply.github.com>
2022-09-21 14:25:41 +0300

    Add files via upload

--- parsalep.py
+++ parsalep.py
@@ -1,6 +1,7 @@
-a=float(input("Введіть довжину: "))
-b=float(input("Введіть ширину: "))
-h=float(input("Введіть висоту: "))
+import sys
+a = float(sys.argv[1]) 
+b = float(sys.argv[2]) 
+h = float(sys.argv[3]) 
 S=2*(a*b+a*h+b*h)
 V=a*b*h
 print("Площа= ",S)
